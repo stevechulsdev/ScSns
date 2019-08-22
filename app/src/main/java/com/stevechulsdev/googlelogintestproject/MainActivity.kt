5f74097816055b9dab7662d35e4b83fe19e2f7d4
김상철 <stevechulsdev@gmail.com>
2019-08-23 01:56:34 +0900

    add 빈 google-services.json file

--- app/src/main/java/com/stevechulsdev/googlelogintestproject/MainActivity.kt
+++ app/src/main/java/com/stevechulsdev/googlelogintestproject/MainActivity.kt
@@ -4,7 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.auth.FirebaseUser
+//import com.google.firebase.auth.FirebaseUser
 import com.stevechulsdev.scsns.ScSnsGoogle
 import kotlinx.android.synthetic.main.activity_main.*
 
